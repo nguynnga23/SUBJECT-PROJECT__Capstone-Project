3457b9ef46268529aa7a759b8be84ce439f89d9c
youthofmay123 <nhathuy39337343@gmail.com>
2025-06-22 15:25:00 +0700

    chore: gitinore download/ and _pycache

--- crawler/crawler/config.py
+++ crawler/crawler/config.py
@@ -1,5 +1,9 @@
 import os
+from dotenv import load_dotenv
 from typing import Final
+
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 UNIFEED_CMS_GRAPHQL_HOST: Final[str] = os.getenv('UNIFEED_CMS_GRAPHQL_HOST') or 'localhost'
 UNIFEED_CMS_GRAPHQL_PORT: Final[str] = os.getenv('UNIFEED_CMS_GRAPHQL_PORT') or '1337'
