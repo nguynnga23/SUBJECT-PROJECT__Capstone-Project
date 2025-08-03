77d9b79f1a5dadd0f39a81c99461d15ade07226e
Nguyen Nga <nguyennga200x@gmail.com>
2025-08-03 15:22:52 +0700

    feat: add repository interfaces for Article, Category, CrawlerConfig, Department, DepartmentWebsite, Permission, Role, and User entities

---
+++ backend/src/main/java/com/backend/repository/DepartmentRepository.java
@@ -0,0 +1,11 @@
+package com.backend.repository;
+
+import com.backend.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, UUID> {
+}
---
+++ backend/src/main/java/com/backend/repository/DepartmentWebsiteRepository.java
@@ -0,0 +1,8 @@
+package com.backend.repository;
+
+
+import com.backend.entity.DepartmentWebsite;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DepartmentWebsiteRepository extends JpaRepository<DepartmentWebsite, Integer> {
+}
